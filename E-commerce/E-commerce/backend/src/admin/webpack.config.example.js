1b1cc26aa8421e5f38adb8d348aaa7db45264724
Abhishek Gupta <abskgupta11@gmail.com>
2023-02-26 13:47:52 +0530

    E-commerce task pushed

---
+++ E-commerce/E-commerce/backend/src/admin/webpack.config.example.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/* eslint-disable no-unused-vars */
+module.exports = (config, webpack) => {
+  // Note: we provide webpack above so you should not `require` it
+  // Perform customizations to webpack config
+  // Important: return the modified config
+  return config;
+};
+
+
+// 136b3f76f9e0b214d57dd9348991dc513ed4f9391bb5ee8b6cb0ce41991fe35122469851470a5b93897597501ae045b343e19921c0182816b8763d4ec977ceed86b5cc13e3d1fdacd4909ff60f47d94be6ce10a9efba102026ee65095bf3e958ca702ec0fe9e7158a0ac17e8317c07544c3f9eabcd8258cbe818164d6878faaa
